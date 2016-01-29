b5b64ed28f94c9e967174c17d2a4f9bd0613407c
gowa66 <gowa66at@gmail.com>
2016-01-29 19:31:39 +0200

    Init project

---
+++ notelist/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+# Create your models here.
+class Note(models.Model):
+    def __unicode__(self):
+        return self.text
+
+    text = models.TextField(verbose_name=_("text field"),)
+
+    class Meta:
+        verbose_name_plural = _("text notes")
+    
---
+++ notelist/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from models import Note
+
+# Create your views here.
+class NoteListView(ListView):
+	template_name = "home.html"
+	model = Note
