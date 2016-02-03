86780f693c8168d1f804470e81a0b60ed404e8e4
gowa66 <gowa66at@gmail.com>
2016-02-03 17:19:15 +0200

    Heroku things

--- django_tlr/wsgi.py
+++ django_tlr/wsgi.py
@@ -13,6 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_tlr.settings")
 
-from dj_static import Cling
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
