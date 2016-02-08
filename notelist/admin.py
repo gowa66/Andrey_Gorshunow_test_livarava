6f827241b0a1e98f0a72395e66daf5539a6c4951
gowa66 <gowa66at@gmail.com>
2016-02-08 13:52:41 +0200

    fixes

--- notelist/admin.py
+++ notelist/admin.py
@@ -1,27 +1,22 @@
 from django.contrib import admin
+from models import Note, Book
 
-from django.core.urlresolvers import reverse
-from models import Note, Book, NoteBook
-from forms import NoteForm
-
-# Register your models here.
+class NoteBookAdmin(admin.TabularInline):
+    model = Book.notes.through
+ 
+ 
 class NoteAdmin(admin.ModelAdmin):
-    form = NoteForm
+    inlines = [
+        NoteBookAdmin,
+    ]
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('id', 'notes_written')
-
-    def notes_written(self, obj):
-        redirect_url = '/admin/notelist/note/'
-        extra = "?book__id__exact=%d" % obj.id
-        return "<a href='%s'>Notes in this book</a>" % (redirect_url + extra)
-    notes_written.allow_tags = True
+    inlines = [
+        NoteBookAdmin,
+    ]
+    exclude = ('notes',)
 
 
-class NoteBookAdmin(admin.ModelAdmin):
-    raw_id_fields = ['note', 'book']
-
-admin.site.register(Book, BookAdmin)
-admin.site.register(NoteBook, NoteBookAdmin) 
 admin.site.register(Note, NoteAdmin)
+admin.site.register(Book, BookAdmin)
--- notelist/models.py
+++ notelist/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from django.db.models.signals import post_delete
-from django.dispatch import receiver
+from django.db.models.signals import pre_delete
 
 def image_directory_path(instance, filename):
     return 'note_{0}/{1}'.format(instance, filename)
@@ -25,24 +24,16 @@ class Note(models.Model):
          app_label = 'notelist'
 
 class Book(models.Model):
-    notes = models.ManyToManyField(Note, through="NoteBook")
+    title = models.CharField(max_length=100)
+    notes = models.ManyToManyField(Note)
+    
+    def __unicode__(self):
+        return self.title
 
 
-class NoteBook(models.Model):
-    note = models.ForeignKey(Note)
-    book = models.ForeignKey(Book)
-
-
-@receiver(post_delete, sender=NoteBook)
-def delete_book_without_notes(sender, **kwargs):
-    notebook = kwargs['instance']
-
-    try:
-        book = notebook.book
-
-        if NoteBook.objects.filter(book=book).count() == 0:
+def note_delete(sender, instance, *args, **kwargs):
+    for book in instance.book_set.all():
+        if book.notes.count() == 1:
             book.delete()
-    except Book.DoesNotExist:
-        pass
 
-
+pre_delete.connect(note_delete, sender=Note)
