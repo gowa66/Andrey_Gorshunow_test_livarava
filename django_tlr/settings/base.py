f7826fb2df4859a98ef9df53735128728ace0726
gowa66 <gowa66at@gmail.com>
2016-02-10 00:17:38 +0200

    heroku

--- django_tlr/settings/base.py
+++ django_tlr/settings/base.py
@@ -101,7 +101,7 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_in_env", "static_root")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static_in_pro", "our_static"),
     # os.path.join(BASE_DIR, "static_in_env"),
@@ -109,6 +109,6 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_in_env", "media_root")
 
 CORS_ORIGIN_ALLOW_ALL = True
